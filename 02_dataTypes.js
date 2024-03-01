f0d8799ff17993f21ff9535850021224b35967d9
Yash Sikhwal Nagla <naglayash@gmail.com>
2024-03-01 14:04:28 +0530

    Content Added

---
+++ 02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" //Treats all JS code as newer version
+
+// alert("Hello ! Welcome to my website!") //We are using node.js not browser that's why this code doesn't run.
+
+/*
+console.log(3
+    +
+    3); // Code on the same line is executed together, but it not a good practice beacuse it has less  readability and can cause bugs if you forget any part of code.
+*/
+console.log("Yash");
+
+let firstName = "Yash"
+let  age = 21
+let isLoggedIn = true
+
+console.log([firstName, age, isLoggedIn]);
+
+// number => 2 to power 53
+// bigInt
+// string => "Try to use double quots"
+// boolean => true or false
+// null => standalone value or represents a value with no value at all
+// undefined => used when variable has not been assigned
+// object => collection of properties and methods
+// symbol => 
