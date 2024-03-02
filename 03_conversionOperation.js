850b1cee84b2b299f3b2c64807405448bfc038b9
yashnagla <naglayash@gmail.com>
2024-03-02 16:25:20 +0530

    Conversion Operation

---
+++ 03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = "33"
+// let score = "33abd"
+// let score = null
+// let score = undefined
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// "true" => 1
+// "false" => 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = ""
+// let isLoggedIn = "Yash"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
