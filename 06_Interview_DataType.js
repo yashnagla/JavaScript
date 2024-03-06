93b7b81818517cbbcf36c74a80b1cdf0afc8033c
yashnagla <naglayash@gmail.com>
2024-03-06 21:50:22 +0530

    Created

---
+++ 06_Interview_DataType.js
@@ -0,0 +1,21 @@
+// Primitive types :- In JavaScript, there are 7 types :- String, Number, Boolearn, null, undefined, symbol and BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+
+const bigNumber =  1234567789098764322456778098765432123456790987564321n
+
+
+// Reference (Non Primitive) Type :- In JavaScript, there are 3 types :- Array, Objects and Function
+
+const heros = [""]
